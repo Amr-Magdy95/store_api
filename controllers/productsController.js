ad1a8da6240cfab58de78e43a2ddf047fcf2a9f9
Amr-Magdy95 <37376273+Amr-Magdy95@users.noreply.github.com>
2023-11-22 11:49:35 +0200

    Feature: Added products queries

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,7 +1,57 @@
-exports.getAllProducts = async (req, res) => {
-  res.send("hello");
-};
+const Product = require("../models/Product");
 
-exports.getAllProductsStatic = async (req, res) => {
-  res.send("static");
+exports.getAllProducts = async (req, res) => {
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
+  const queryObject = {};
+  if (featured) queryObject.featured = featured === "true" ? true : false;
+  if (company) queryObject.company = company;
+  if (name) queryObject.name = { $regex: name, $options: "i" };
+
+  let result = Product.find(queryObject);
+
+  // sort
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createdAt");
+  }
+
+  // fields
+  if (fields) {
+    const fieldsList = fields.split(",").join(" ");
+    result = result.select(fieldsList);
+  }
+
+  // Numeric Filters
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      ">=": "$gte",
+      "<": "$lt",
+      "<=": "$lte",
+      "=": "$eq",
+    };
+    const regExp = /\b(<|>|<=|>=|=)\b/g;
+    let filters = numericFilters.replace(
+      regExp,
+      (match) => `-${operatorMap[match]}-`
+    );
+    const options = ["price", "rating"];
+    filters.split(",").forEach((item) => {
+      const [field, op, value] = item.split("-");
+      if (options.includes(field)) {
+        queryObject[field] = { [op]: Number(value) };
+      }
+    });
+  }
+
+  // pages
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+  result.skip(skip).limit(limit);
+
+  const products = await result;
+  res.status(200).json({ nbHits: products.length, products });
 };
